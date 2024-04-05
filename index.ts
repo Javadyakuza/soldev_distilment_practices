1e2642a60ab80df30b6247b8649158946738391c
javadyakuza <javadyakuzaa@gmail.com>
2024-04-05 17:15:38 +0330

    added the read from custom program

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { gen_kp, read_kp } from "./src/generate_kp";
 import { load_and_get_bal } from "./src/read_from_solana";
-import {transfer_sol} from "./src/creating_tx";
+import {read_from_counter, transfer_sol} from "./src/creating_tx";
 
 async function main () {
     gen_kp();
@@ -12,6 +12,7 @@ async function main () {
     console.log("this is the balance: ", bal);
     
     await transfer_sol();
+    await read_from_counter();
 
 }
 main();
--- src/creating_tx.ts
+++ src/creating_tx.ts
@@ -1,8 +1,12 @@
-import { airdropIfRequired } from "@solana-developers/helpers";
+import { airdropIfRequired, getKeypairFromEnvironment } from "@solana-developers/helpers";
 import { LAMPORTS_PER_SOL, Connection, SystemProgram, Transaction, clusterApiUrl, sendAndConfirmTransaction, PublicKey, Keypair } from "@solana/web3.js";
 import { read_kp } from "./generate_kp";
+import * as web3 from "@solana/web3.js";
+import "dotenv/config"
+
 
 export async function transfer_sol() {
+    // see the documentation to learn how to run a local cluster
     let conn = new Connection("http://localhost:8899", "confirmed");
 
     console.log(
@@ -61,3 +65,52 @@ export async function transfer_sol() {
 }
 
 
+export async function read_from_counter(): Promise<String> {
+    const connection = new Connection(clusterApiUrl("devnet"));
+
+    // let conn = new Connection("http://localhost:8899", "confirmed");
+
+    // loading the payer of the transaction
+    let payer = await read_kp();
+
+    await airdropIfRequired(
+        connection,
+        payer.publicKey,
+        1 * LAMPORTS_PER_SOL,
+        0.5 * LAMPORTS_PER_SOL,
+    );
+    // static addresses
+    const PROGRAM_ADDRESS = new web3.PublicKey('ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa')
+    const PROGRAM_DATA_ADDRESS =  new web3.PublicKey('Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod');
+
+    const transaction = new web3.Transaction()
+    const programId = new web3.PublicKey(PROGRAM_ADDRESS)
+    const ProgramDataId = new web3.PublicKey(PROGRAM_DATA_ADDRESS)
+
+    // creating the instruction  
+    const instruction = new web3.TransactionInstruction({
+      keys: [
+        {
+          pubkey: ProgramDataId,
+          isSigner: false,
+          isWritable: true
+        },
+      ],
+      programId
+    })
+
+    // adding the instruction to the transaction object
+    transaction.add(instruction)
+    
+
+
+    const signature = await web3.sendAndConfirmTransaction(
+      connection,
+      transaction,
+      [payer]
+    )
+    
+    console.log(`✅ Transaction completed! Signature is ${signature}`)
+    
+    return signature;
+}
